b76ae66211306dfc66f315669999b0ef766731f1
aBruisedReed <snare909@gmail.com>
2021-09-27 21:17:19 +0900

    feat: title, favicon 수정

--- src/DevTool.js
+++ src/DevTool.js
@@ -11,27 +11,27 @@ const DevToolBlock = styled.div`
 
 const UsersInfo = [{
   id: '123456789012345678111',
-  displayName: '유현덕',
+  displayName: '교강용',
   image: 'https://picjumbo.com/wp-content/uploads/man-washing-white-car-in-a-self-service-car-wash-free-photo-1080x720.jpg'
   },
   {
     id: '123456789012345678112',
-    displayName: '조맹덕',
-    image: 'https://picjumbo.com/wp-content/uploads/welcoming-seljalandsfoss-waterfall-1080x737.jpg'
+    displayName: '황산',
+    image: 'https://picjumbo.com/wp-content/uploads/black-styled-minimal-office-things-room-for-text-2210x1473.jpg'
   },
   {
     id: '123456789012345678113',
-    displayName: '관운장',
+    displayName: '마영웅',
     image: 'https://picjumbo.com/wp-content/uploads/woman-riding-a-bicycle-and-wearing-a-helmet-free-photo-1080x1620.jpg'
   },
   {
     id: '123456789012345678114',
-    displayName: '장익덕',
+    displayName: '강건마',
     image: 'https://picjumbo.com/wp-content/uploads/man-standing-on-pragues-charles-bridge-in-sepia-tone-1080x720.jpg'
   },
   {
     id: '123456789012345678115',
-    displayName: '조자룡',
+    displayName: '전사독',
     image: 'https://picjumbo.com/wp-content/uploads/backcountry-skier-in-norway-1080x1620.jpg'
   }];
 
