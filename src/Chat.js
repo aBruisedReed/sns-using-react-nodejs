db8aa4f3599e86e24cb4d733899b7a8891da963c
aBruisedReed <snare909@gmail.com>
2021-09-28 01:51:02 +0900

    fix: chat, null, 추가

--- src/Chat.js
+++ src/Chat.js
@@ -222,7 +222,7 @@ const chatReducer = (state, action) => {
     case 'CHAT_OFF':
       return initialState;
     case 'CHAT_UPDATE':
-      if(state.chatLog === null) {
+      if(state.chatLog.length === 0) {
         return { ...state, chatLog: [action.newChatLog] };
       }
       return { ...state, chatLog: state.chatLog.concat(action.newChatLog) };
